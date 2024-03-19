39c401a70266aa04bbc88c7306e349a5dbee31cb
lhy9496 <dlghdus1949@gmail.com>
2024-03-19 18:09:03 +0900

    project 3rd

---
+++ My_Project3rd/src/Member/MemberMenu.java
@@ -0,0 +1,7 @@
+package Member;
+
+public class MemberMenu {
+	public void mainMenu() {
+		
+	}
+}
---
+++ My_Project3rd/src/Run/Run.java
@@ -0,0 +1,9 @@
+package Run;
+
+import Member.MemberMenu;
+
+public class Run {
+	public static void main(String[] args) {
+		new MemberMenu().mainMenu();
+	}
+}
